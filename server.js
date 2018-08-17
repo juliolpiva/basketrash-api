1a3212f7c10785df959792abaf7abeae07967cc7
JulioLP <juliolpiva@hotmail.com>
2018-08-16 23:35:50 -0300

    change hello world!

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Basketrash!');
-});
+port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Basketrash running on port 3000!');
 })
+
+app.get('/', function (req, res) {
+  res.json({ hello: 'basketrash!' });
+});
